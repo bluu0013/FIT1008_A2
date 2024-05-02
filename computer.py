5bfdb1abb36ca803290244d7802187035d7242e9
Brandon <bluu0013@student.monash.edu>
2024-05-02 16:37:43 +1000

    Completed Task 5 w/ complexity analysis

--- computer.py
+++ computer.py
@@ -9,3 +9,42 @@ class Computer:
     hacking_difficulty: int
     hacked_value: int
     risk_factor: float
+
+    def __lt__(self,other):
+        if self.hacking_difficulty == other.hacking_difficulty:
+            if self.risk_factor == other.risk_factor:
+                return self.name < other.name
+            else:
+                return self.risk_factor < other.risk_factor
+        else:
+            return self.hacking_difficulty < other.hacking_difficulty
+    
+    def __gt__(self,other):
+        if self.hacking_difficulty == other.hacking_difficulty:
+            if self.risk_factor == other.risk_factor:
+                return self.name < other.name
+            else:
+                return self.risk_factor > other.risk_factor
+        else:
+            return self.hacking_difficulty > other.hacking_difficulty
+        
+    def __le__(self,other):
+        if self.hacking_difficulty == other.hacking_difficulty:
+            if self.risk_factor == other.risk_factor:
+                return self.name <= other.name
+            else:
+                return self.risk_factor <= other.risk_factor
+        else:
+            return self.hacking_difficulty <= other.hacking_difficulty
+
+    def __ge__(self,other):
+        if self.hacking_difficulty == other.hacking_difficulty:
+            if self.risk_factor == other.risk_factor:
+                return self.name >= other.name
+            else:
+                return self.risk_factor >= other.risk_factor
+        else:
+            return self.hacking_difficulty >= other.hacking_difficulty
+           
+    def __eq__(self,other):
+        return (self.name == other.name)
--- computer_organiser.py
+++ computer_organiser.py
@@ -1,15 +1,37 @@
 from __future__ import annotations
-
 from computer import Computer
-
+from algorithms.mergesort import *
+from algorithms.binary_search import *
 
 class ComputerOrganiser:
 
     def __init__(self) -> None:
-        raise NotImplementedError()
+        self.computers = []
 
     def cur_position(self, computer: Computer) -> int:
-        raise NotImplementedError()
+        """
+        Uses binary search to find the index of the given computer in the list.
+        Will raise exception if computer does not exist in current list.
+
+        :complexity: O(log(N)) where N is the length of current list of computers.
+        """
+        res = binary_search(self.computers, computer)
+        try:
+            if self.computers[res] == computer:
+                return res
+            else:
+                raise KeyError(computer)
+        except:
+            raise KeyError(computer)
 
     def add_computers(self, computers: list[Computer]) -> None:
-        raise NotImplementedError()
+        """
+        Adds new computers to list, then uses mergesort to rank them.
+
+        :complexity: O( (M+N)*(log(M+N)) ) where M is the length of the input
+                                            and N is the length of the current list
+        """
+        for i in computers:
+            self.computers.append(i)
+
+        self.computers = mergesort(self.computers)
