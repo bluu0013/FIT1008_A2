d72ef355eecf944554e1fca4a57c3cb75c1f74de
Brandon <bluu0013@student.monash.edu>
2024-05-02 21:33:27 +1000

    Completed complexity for Task 4

--- infinite_hash_table.py
+++ infinite_hash_table.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import Generic, TypeVar
-
+from algorithms.mergesort import *
 from data_structures.referential_array import ArrayR
 
 K = TypeVar("K")
@@ -23,6 +23,7 @@ class InfiniteHashTable(Generic[K, V]):
 
     def __init__(self, level: int = 0) -> None:
         self.array: ArrayR[tuple[K, V] | None] = ArrayR(self.TABLE_SIZE)
+        self.keys = []
         self.count = 0
         self.level = level
     
@@ -35,6 +36,7 @@ class InfiniteHashTable(Generic[K, V]):
         """
         Get the value at a certain key
 
+        :complexity: O(hash(key) * level) where the level indicates what level of hash table the item is in.
         :raises KeyError: when the key doesn't exist.
         """
         pos = self.hash(pos)
@@ -51,12 +53,17 @@ class InfiniteHashTable(Generic[K, V]):
     def __setitem__(self, key: K, value: V) -> None:
         """
         Set an (key, value) pair in our hash table.
+
+        :Best case complexity: O(hash(key)) when position is empty.
+        :Worst case complexity: O(hash(key1) * hash(key2)) where key1 and key2 are two colliding items.
         """
         pos = self.hash(key)
+        self.keys.append(key)
         self.count += 1
 
         if self.array[pos] is None:
             self.array[pos] = (key,value)
+            
         else:
             old_key, old_value  = self.array[pos]
             new_level = self.level + 1
@@ -68,17 +75,18 @@ class InfiniteHashTable(Generic[K, V]):
     def __delitem__(self, key: K) -> None:
         """
         Deletes a (key, value) pair in our hash table.
-
+        
+        :complexity: O(hash(key) * level) where the level indicates what level of hash table the item is in.
         :raises KeyError: when the key doesn't exist.
         """
         pos = self.hash(key)
-        self.count -= 1
 
         if self.array[pos] is None:
             raise KeyError(key)
         else:
-            if self.array[pos][0] == key:
+            if self.array[pos][0] == key or type(self.array[pos][1]) is int:
                 self.array[pos] = None
+                self.count -= 1
             else:
                 sub_table = self.array[pos][1]
                 del sub_table[key]
@@ -98,6 +106,7 @@ class InfiniteHashTable(Generic[K, V]):
         """
         Get the sequence of positions required to access this key.
 
+        :complexity: O(hash(key) * level) where the level indicates what level of hash table the item is in.
         :raises KeyError: when the key doesn't exist.
         """
         res = []
@@ -106,10 +115,12 @@ class InfiniteHashTable(Generic[K, V]):
         if self.array[pos] is None:
             raise KeyError(key)
         else:
-            res.append(pos)
-            if self.array[pos][0] == key:
+
+            if self.array[pos][0] == key or type(self.array[pos][1]) is int:
+                res.append(pos)
                 return res
             else:
+                res.append(pos)
                 sub_table = self.array[pos][1]
                 return res + sub_table.get_location(key)
 
@@ -129,5 +140,8 @@ class InfiniteHashTable(Generic[K, V]):
     def sort_keys(self, current=None) -> list[str]:
         """
         Returns all keys currently in the table in lexicographically sorted order.
+
+        :complexity: O(Nlog(N)) where N is the number of keys in list.
         """
-        raise NotImplementedError()
+        self.keys = mergesort(self.keys)
+        return self.keys    
