d74165aa9d20f695418478026e63a8f737d3a3e7
Brandon <bluu0013@student.monash.edu>
2024-05-03 21:47:14 +1000

    Added Route and Virus files

--- route.py
+++ route.py
@@ -24,9 +24,16 @@ class RouteSplit:
     bottom: Route
     following: Route
 
-    def remove_branch(self) -> RouteStore:
+    def remove_branch(self) -> RouteStore:  #leaves the top of a split and should just remove a series.
         """Removes the branch, should just leave the remaining following route."""
-        raise NotImplementedError()
+        
+        newRouteStore = RouteStore(self.top)
+
+        return newRouteStore
+        
+
+        
+
 
 @dataclass
 class RouteSeries:
@@ -45,34 +52,58 @@ class RouteSeries:
         Returns a route store which would be the result of:
         Removing the computer at the beginning of this series.
         """
-        raise NotImplementedError()
+        self.computer = None
+
+        return RouteStore
 
     def add_computer_before(self, computer: Computer) -> RouteStore:
         """
         Returns a route store which would be the result of:
         Adding a computer in series before the current one.
         """
-        raise NotImplementedError()
+        theSecondComputer = RouteSeries(self.computer,self.following)
+
+        fullRoute = RouteSeries(computer,theSecondComputer)
+
+        return fullRoute
 
     def add_computer_after(self, computer: Computer) -> RouteStore:
         """
         Returns a route store which would be the result of:
         Adding a computer after the current computer, but before the following route.
         """
-        raise NotImplementedError()
+        theFirstComputer = RouteSeries(self.computer,self.following)
+
+        fullRoute = RouteSeries(computer,theFirstComputer)
+
+        return fullRoute
 
     def add_empty_branch_before(self) -> RouteStore:
         """Returns a route store which would be the result of:
         Adding an empty branch, where the current routestore is now the following path.
         """
-        raise NotImplementedError()
+        theSeriesRoute = RouteSeries(self.computer,self.following)
+
+        theAddedBranch = RouteSeries(None,theSeriesRoute)
+
+        return theAddedBranch
 
     def add_empty_branch_after(self) -> RouteStore:
         """
         Returns a route store which would be the result of:
         Adding an empty branch after the current computer, but before the following route.
         """
-        raise NotImplementedError()
+        
+
+        theHead = RouteSeries(self.computer,emptybranch)
+
+        thefollowing = RouteSeries(None,self.following)
+
+        emptybranch = RouteSeries(None,thefollowing)
+
+        
+
+        return theHead
 
 
 RouteStore = Union[RouteSplit, RouteSeries, None]
@@ -88,19 +119,60 @@ class Route:
         Returns a *new* route which would be the result of:
         Adding a computer before everything currently in the route.
         """
-        raise NotImplementedError()
+        theComputer = RouteSeries(computer,None)
+
+        return Route(theComputer,self.store)
+
+
 
     def add_empty_branch_before(self) -> Route:
         """
         Returns a *new* route which would be the result of:
         Adding an empty branch before everything currently in the route.
         """
-        raise NotImplementedError()
+        theEmptyBranch = RouteSeries
+
+        return Route(theEmptyBranch,self.store)
 
     def follow_path(self, virus_type: VirusType) -> None:
         """Follow a path and add computers according to a virus_type."""
-        raise NotImplementedError()
+        #the time complexity is O(n + m) where n is the amount of branches and m is the amount of computers
+
+        while type(self.store.following) != None: 
+            
+            virus_type.add_computer
+            virus_type.select_branch
+        
+
+
 
     def add_all_computers(self) -> list[Computer]:
         """Returns a list of all computers on the route."""
-        raise NotImplementedError()
+        #O(n) where n is the amount of branches traversed
+        
+        added_computers = list                              
+        branch = self.store
+
+        if type(branch.following) == None:
+            
+            return added_computers.append(branch.computer)
+        
+        else:
+            added_computers.append(branch.computer)
+            branch = branch.following
+            added_computers.extend(self.add_all_computers)
+        
+        """
+        this is for if the list needs no nones in it but otherwise use added_computer 
+        no_nones_added_computers = list
+
+        for element in added_computers:         #O(n) where n is the length of added computers
+            if element != None:
+                no_nones_added_computers.append(element)
+        return no_nones_added_computers
+                
+        """
+        return added_computers
+    
+
+        
--- virus.py
+++ virus.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from computer import Computer
-from route import Route, RouteSeries
+from route import Route, RouteSeries, RouteSplit
 from branch_decision import BranchDecision
+from data_structures.stack_adt import Stack
+
+#if unspecified then the complexity for that line is O(1)
 
 
 class VirusType(ABC):
@@ -15,8 +18,8 @@ class VirusType(ABC):
 
     @abstractmethod
     def select_branch(self, top_branch: Route, bottom_branch: Route) -> BranchDecision:
-        raise NotImplementedError()
-
+        
+        pass
 
 class TopVirus(VirusType):
     def select_branch(self, top_branch: Route, bottom_branch: Route) -> BranchDecision:
@@ -61,7 +64,65 @@ class RiskAverseVirus(VirusType):
         """
         This virus is risk averse and likes to choose the path with the lowest risk factor.
         """
-        raise NotImplementedError()
+        #all lines in this class are O(1)
+
+        top_route = type(top_branch.store)                                  
+        bot_route = type(bottom_branch.store) 
+        top_comp = top_branch.store.computer
+        bot_comp = bottom_branch.store.computer
+        
+        
+        if (top_route == RouteSplit and bot_route != RouteSplit):
+            return BranchDecision.TOP
+        if (top_route != RouteSplit and bot_route == RouteSplit):
+            return BranchDecision.BOTTOM
+        
+        if (top_comp.risk_factor == 0.0 and bot_comp.risk_factor != 0.0):
+            return BranchDecision.TOP
+        if (bot_comp.risk_factor == 0.0 and top_comp.risk_factor != 0.0):
+            return BranchDecision.BOTTOM
+        
+        if top_comp.risk_factor == 0.0 and bot_comp.risk_factor == 0.0:
+            
+            if top_comp.hacking_difficulty < bot_comp.hacking_difficulty:
+                return BranchDecision.TOP
+            
+            if bot_comp.hacking_difficulty < top_comp.hacking_difficulty:
+                return BranchDecision.BOTTOM
+            
+            if top_comp.hacking_difficulty == bot_comp.hacking_difficulty:
+
+                top_hacked_combine = top_comp.hacking_difficulty + top_comp.hacked_value/2 
+                bot_hacked_combine = bot_comp.hacking_difficulty + bot_comp.hacked_value/2
+
+                if top_comp.risk_factor > 0:
+                
+                    top_hack_risk_combine = top_hacked_combine / top_comp.risk_factor
+
+                    if top_hack_risk_combine > bot_hack_risk_combine:
+                    
+                        return BranchDecision.TOP
+
+                if bot_comp.risk_factor > 0:
+
+                    bot_hack_risk_combine = bot_hacked_combine / bot_comp.risk_factor
+                    
+                    if bot_hack_risk_combine > top_hack_risk_combine:
+
+                        return BranchDecision.BOTTOM
+                   
+               
+                    if top_hack_risk_combine == bot_hack_risk_combine:
+        
+                        if top_comp.risk_factor < bot_comp.risk_factor:
+                            return BranchDecision.TOP
+                        if bot_comp.risk_factor < top_comp.risk_factor:
+                            return BranchDecision.BOTTOM
+                
+                    return BranchDecision.STOP
+
+        return BranchDecision.TOP
+            
 
 
 class FancyVirus(VirusType):
@@ -71,4 +132,59 @@ class FancyVirus(VirusType):
         """
         This virus has a fancy-pants and likes to overcomplicate its approach.
         """
-        raise NotImplementedError()
+        #unless specified the lines have a time complexity of O(1)
+
+        calc_string = FancyVirus.CALC_STR
+
+        calc_string_list = calc_string.split()  #O(n) where n is the length of the input string (calc_string)
+
+        temp_stack = Stack
+
+        for element in calc_string_list:        #O(n) where n is the length of the input string (calc_string)
+
+            if element not in "/*+-":
+                temp_stack.push(int(element))
+            
+            else:
+                right = temp_stack.pop() 
+                left = temp_stack.pop() 
+                    
+                
+                if element == '+': 
+                    temp_stack.push(left + right) 
+                    
+                elif element == '-': 
+                    temp_stack.push(left - right) 
+                    
+                elif element == '*': 
+                    temp_stack.push(left * right) 
+                    
+                elif element == '/': 
+                    temp_stack.push(int(left / right)) 
+                
+            
+        threshold =  temp_stack.pop() 
+
+
+        top_route = type(top_branch.store) 
+        bot_route = type(bottom_branch.store) 
+        top_comp = top_branch.store.computer
+        bot_comp = bottom_branch.store.computer
+        
+        if (top_route == RouteSplit and bot_route != RouteSplit):
+            return BranchDecision.TOP
+        if (top_route != RouteSplit and bot_route == RouteSplit):
+            return BranchDecision.BOTTOM
+        
+        if top_comp != None and bot_comp != None:
+
+
+            if top_comp.hacked_value < threshold:
+                BranchDecision.TOP
+
+            if bot_comp.hacked_value > threshold:   
+                BranchDecision.BOTTOM
+            
+            return BranchDecision.STOP
+        
+        return BranchDecision.TOP
